b1b29ddd1bfdd364aeec1f532ae847716aa5e1e6
unknown <kasperkasperfish@gmail.com>
2019-02-19 21:32:22 +0100

    add claim all

--- src/pages/custodian/claim-pay.vue
+++ src/pages/custodian/claim-pay.vue
@@ -1,8 +1,10 @@
 <template>
   <q-page class="q-pa-md">
-    <div class="q-display-1 text-text1 q-mb-md">Claim Payments {{totalPayAmount}}</div>
+    <div class="q-display-1 text-text1 q-mb-md">Claim Payments</div>
     <div class="row">
       <div class="bg-bg1 round-borders shadow-5 col-xs-12 col-md-6">
+        <div>total: {{totalPayAmount}}</div>
+        <div>payments: {{pendingpay.length}}</div>
         <div v-if="pendingpay.length">
           <q-item v-for="(pay, i) in pendingpay" :key="`pay_id_${i}`">
             <q-item-side left>{{pay.key}}</q-item-side>
@@ -14,6 +16,7 @@
               <q-btn label="claim" color="primary" @click="claimpay(pay.key)"/>
             </q-item-side>
           </q-item>
+          <q-btn label="claim all" @click="claimAll" />
         </div>
         <div v-else>
           You do not have any pending payments.
@@ -46,6 +49,7 @@ export default {
       // getactiveCustodians: 'api/getActiveCustodians',
       // getAccountName: 'account/getAccountName'
     }),
+
     totalPayAmount(){
       if(!this.pendingpay.length) return 0;
 
@@ -73,6 +77,22 @@ export default {
         
       }
         
+    },
+    async claimAll(){
+      const contract = this.$configFile.get('custodiancontract');
+      let actions = this.pendingpay.map(pp=>{
+        return {
+          account: contract, 
+          name: 'claimpay',
+          data: {
+            payid: pp.key, 
+          }
+        };
+      });
+      let result = await this.$store.dispatch('user/transact', {actions: actions} );
+      if(result){
+        
+      }
     },
     async getClaimPay(){
       this.loading = true;
