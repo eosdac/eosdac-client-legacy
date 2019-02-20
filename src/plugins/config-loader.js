d69daa8617d194d6fde4242cb5c88897292ce1df
unknown <kasperkasperfish@gmail.com>
2019-02-20 11:59:31 +0100

    remove redundant breaks after return statements

--- src/plugins/config-loader.js
+++ src/plugins/config-loader.js
@@ -13,43 +13,30 @@ class configLoader {
     switch (configquery) {
       case 'tokencontract':
         return this.configFile.contracts.token.name;
-        break;
       case 'custodianmemo':
         return this.configFile.contracts.custodian.memo;
-        break;
       case 'custodiancontract':
         return this.configFile.contracts.custodian.name;
-        break;
       case 'dactokensymbol':
         return this.configFile.contracts.token.symbol;
-        break;
       case 'systemtokensymbol':
         return 'EOS';
-        break;
       case 'systemtokencontract':
         return 'eosio.token';
-        break;
       case 'systemmsigcontract':
         return this.configFile.contracts.system_msig.name;
-        break;
       case 'dacmsigcontract':
         return this.configFile.contracts.dac_msig.name;
-        break;
       case 'botcontract':
         return this.configFile.contracts.bot.name;
-        break;
       case 'explorer':
         return this.configFile.external.explorer;
-        break;
       case 'memberclientapi':
         return this.configFile.api.memberclient;
-        break;
       case 'external':
         return this.configFile.external;
-        break;
       default:
         return `***${configquery} not yet subscribed in config-loader***`
-        break;
     }
   }
   
