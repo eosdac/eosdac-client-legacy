126a8d57133137688d969115f9ca6dd5e81940b9
unknown <kasperkasperfish@gmail.com>
2019-03-19 00:08:25 +0100

    more chart wip-ping

--- src/pages/custodian/dac-financials.vue
+++ src/pages/custodian/dac-financials.vue
@@ -2,7 +2,7 @@
   <q-page class="q-pa-md">
 
 
-    <line-chart :chartData="chartData" :options="chartOptions"/>
+    <line-chart ref="linechart" :chartData="chartData" :options="chartOptions"/>
   </q-page>
 </template>
 
@@ -18,7 +18,8 @@ export default {
   data () {
     return {
       chartData:null,
-      chartOptions: {responsive: false}
+      chartOptions: {responsive: true, maintainAspectRatio: false},
+ 
     }
   },
   computed:{
@@ -28,23 +29,39 @@ export default {
 
   },
   methods:{
+    getGradient(){
+      let gradient = this.$refs.linechart.$refs.canvas
+      .getContext('2d')
+      .createLinearGradient(0, 0, 0, 450)
+      gradient.addColorStop(0, 'rgba(74, 18, 137, 0.5)');
+      gradient.addColorStop(0.5, 'rgba(74, 18, 137, 0.25)');
+      gradient.addColorStop(1, 'rgba(74, 18, 137, 0)');
+      return gradient;
+    },
     async getTokenTimeLine(){
-      let query = {account: 'piecesnbitss', contract:'kasdactokens', symbol:'KASDAC', start_block:10000000, end_block:17000000};
+      let query = {account: 'evilmikehere', contract:'eosio.token', symbol:'EOS', start_block:0, end_block:17000000000};
       let res = await this.$store.dispatch('dac/fetchTokenTimeLine', query)
-    }
-  },
-  mounted(){
-    // this.getTokenTimeLine();
-    this.chartData={
-        labels: ['January', 'February'],
-        datasets: [
+      this.chartData ={
+        labels: res.map(p => p.block_num),
+        datasets:[
           {
-            label: 'Data One',
-            backgroundColor: 'blue',
-            data: [1, 2]
+            label: 'evilmikehere',
+            data: res.map(p => p.balance.split(' ')[0]),
+
+            backgroundColor: this.getGradient(),
+            borderColor: 'purple', 
+            pointBackgroundColor: 'none', 
+            borderWidth: 1, 
+            pointBorderColor: 'none',
+            pointRadius: 0,
           }
         ]
       }
+    }
+  },
+  mounted(){
+    this.getTokenTimeLine();
+
     
   }
 }
