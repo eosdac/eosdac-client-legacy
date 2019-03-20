c2b2b30ffaa7f44204622b16a44d86c45db2a0c3
unknown <kasperkasperfish@gmail.com>
2019-03-20 23:38:38 +0100

    add treasuryaccount

--- src/pages/custodian/dac-financials.vue
+++ src/pages/custodian/dac-financials.vue
@@ -23,6 +23,12 @@
       </div>
     </div>
 
+    <div class="col-xs-12 col-lg-6">
+      <div class="bg-bg1 q-pa-md round-borders shadow-5" >
+        <balance-timeline :account="this.$configFile.get('treasuryaccount')"  :contract="this.$configFile.get('systemtokencontract')" :symbol="this.$configFile.get('systemtokensymbol')"  />
+      </div>
+    </div>
+
 
 </div>
 
