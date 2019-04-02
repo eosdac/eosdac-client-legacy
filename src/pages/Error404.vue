ab72156ac7e25ebbae733d771c70c86c3422479b
unknown <kasperkasperfish@gmail.com>
2019-04-02 23:24:12 +0200

    brand 404error page

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,19 +1,36 @@
 <template>
-  <div class="fixed-center text-center">
-    <p>
-      <img src="~assets/sad.svg" style="width:30vw;max-width:150px;" />
-    </p>
-    <p class="text-faded">
-      {{ $t("four_zero_four.sorry_nothing_here") }}<strong>(404)</strong>
-    </p>
-    <q-btn color="secondary" style="width:200px;" @click="$router.push('/')">{{
-      $t("four_zero_four.go_back")
-    }}</q-btn>
+  <div class="fixed full-width full-height bg-bg1">
+    <div class="fixed-center text-center ">
+      <p>
+        <img
+          v-if="getIsDark"
+          src="../extensions/branding/images/logo/logo-notext-light.svg"
+          style="width:30vw;max-width:150px;"
+        />
+        <img
+          v-else
+          src="../extensions/branding/images/logo/logo-notext-dark.svg"
+          style="width:30vw;max-width:150px;"
+        />
+      </p>
+      <p class="text-faded">
+        {{ $t("four_zero_four.sorry_nothing_here") }}<strong>(404)</strong>
+      </p>
+      <q-btn color="primary" style="width:200px;" @click="$router.push('/')">{{
+        $t("four_zero_four.go_back")
+      }}</q-btn>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
-  name: "Error404"
+  name: "Error404",
+  computed: {
+    ...mapGetters({
+      getIsDark: "ui/getIsDark"
+    })
+  }
 };
 </script>
