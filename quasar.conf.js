afa1fd848eecbcf9de5e597f2e551731cc3720a2
unknown <kasperkasperfish@gmail.com>
2019-04-02 19:54:46 +0200

    changes in quasar.conf

--- quasar.conf.js
+++ quasar.conf.js
@@ -31,7 +31,7 @@ module.exports = function (ctx) {
     supportIE: false,
     build: {
       env: {
-        NETWORK: JSON.stringify(process.env.DEFAULT_NETWORK)
+        DEFAULT_NETWORK: JSON.stringify(process.env.DEFAULT_NETWORK)
       },
       scopeHoisting: true,
       vueRouterMode: 'history',
@@ -160,27 +160,27 @@ module.exports = function (ctx) {
         theme_color: '#027be3',
         icons: [
           {
-            'src': 'statics/icons/icon-128x128.png',
+            'src': 'extensions/branding/images/icons/icon-128x128.png',
             'sizes': '128x128',
             'type': 'image/png'
           },
           {
-            'src': 'statics/icons/icon-192x192.png',
+            'src': 'extensions/branding/images/icon-192x192.png',
             'sizes': '192x192',
             'type': 'image/png'
           },
           {
-            'src': 'statics/icons/icon-256x256.png',
+            'src': 'extensions/branding/images/icon-256x256.png',
             'sizes': '256x256',
             'type': 'image/png'
           },
           {
-            'src': 'statics/icons/icon-384x384.png',
+            'src': 'extensions/branding/images/icon-384x384.png',
             'sizes': '384x384',
             'type': 'image/png'
           },
           {
-            'src': 'statics/icons/icon-512x512.png',
+            'src': 'extensions/branding/images/icon-512x512.png',
             'sizes': '512x512',
             'type': 'image/png'
           }
