a898005f11c1e350484413f9e773de10e0417462
unknown <kasperkasperfish@gmail.com>
2019-04-02 20:19:54 +0200

    add dacname to config

--- src/plugins/config-loader.js
+++ src/plugins/config-loader.js
@@ -10,6 +10,8 @@ class ConfigLoader {
 
   get(configquery) {
     switch (configquery) {
+      case "dacname":
+        return this.configFile.DAC_name;
       case "defaultnode":
         return this.configFile.api.default_eos_node;
       case "tokencontract":
