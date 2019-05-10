ef04e633459c56c01ff49de21dbbb64404fd6e9f
unknown <kasperkasperfish@gmail.com>
2019-05-10 20:07:30 +0200

    save

--- src/pages/custodian/dac-financials.vue
+++ src/pages/custodian/dac-financials.vue
@@ -54,7 +54,9 @@
               size="sm"
             />
           </div>
-          <div class="q-pa-md">dsfsdfsdf</div>
+          <div class="q-pa-md">
+            <q-btn label="add" dense color="primary" @click="addToQeue" />
+          </div>
         </div>
       </div>
 
@@ -62,7 +64,7 @@
         <div class="bg-bg1 round-borders shadow-5 overflow-hidden">
           <div class="bg-primary q-pa-sm row justify-between items-center">
             <q-icon name="mdi-inbox-arrow-down" color="text2" size="24px" />
-            <span class="uppercase">trx que ({{ trx_qeue.length }})</span>
+            <span class="uppercase">trx qeue ({{ trx_qeue.length }})</span>
             <help-btn
               content="jqh q shkq sdk dk qskdh qskd"
               title="account.account"
@@ -71,40 +73,62 @@
             />
           </div>
           <div>
-            <q-list dense highlight no-border>
-              <q-item v-for="(trx, i) in trx_qeue" :key="`trx${i}`">
-                <q-item-side left>
-                  <q-btn icon="close" flat dense color="negative" />
-                  <q-spinner color="primary-light" />
-                </q-item-side>
-                <q-item-main>
-                  <q-item-tile class="q-body-1 q-py-xs">
-                    {{ trx.from }} > {{ trx.to }} {{ trx.quantity }}
-                  </q-item-tile>
-                </q-item-main>
-                <q-item-side right>
-                  <q-btn
-                    label="view"
-                    size="sm"
-                    dense
-                    flat
-                    color="info"
-                    class="q-mr-xs"
-                  />
-                  <q-btn
-                    label="send"
-                    size="sm"
-                    flat
-                    dense
-                    color="positive"
-                    @click="proposeTransfer(i)"
-                  />
-                </q-item-side>
-              </q-item>
-            </q-list>
+            <q-scroll-area
+              style="height: 300px; padding-bottom:8px"
+              :thumb-style="getThumbStyle()"
+              :delay="1500"
+            >
+              <q-list dense highlight no-border>
+                <q-item v-for="(trx, i) in trx_qeue" :key="`trx${i}`">
+                  <q-item-side left>
+                    <q-btn
+                      v-if="trx.status == 0"
+                      icon="close"
+                      flat
+                      dense
+                      color="negative"
+                      @click="trx_qeue.splice(i, 1)"
+                    />
+                    <q-spinner v-if="trx.status == 1" color="primary-light" />
+                    <q-btn
+                      v-if="trx.status == 2"
+                      icon="check"
+                      flat
+                      dense
+                      color="positive"
+                    />
+                  </q-item-side>
+                  <q-item-main>
+                    <q-item-tile class="q-body-1 q-py-xs">
+                      {{ trx.from }} > {{ trx.to }} {{ trx.quantity }}
+                    </q-item-tile>
+                  </q-item-main>
+                  <q-item-side right>
+                    <q-btn
+                      label="view"
+                      size="sm"
+                      dense
+                      flat
+                      color="info"
+                      class="q-mr-xs"
+                    />
+                    <q-btn
+                      v-if="trx.status == 0"
+                      label="send"
+                      size="sm"
+                      flat
+                      dense
+                      color="positive"
+                      @click="proposeTransfer(i)"
+                    />
+                  </q-item-side>
+                </q-item>
+              </q-list>
+            </q-scroll-area>
           </div>
         </div>
       </div>
+      <!-- end trx qeue -->
     </div>
   </q-page>
 </template>
@@ -114,6 +138,7 @@ import { mapGetters } from "vuex";
 import balanceTimeline from "components/ui/balance-timeline";
 import helpBtn from "components/controls/help-btn";
 import xspan from "components/ui/xspan";
+import { colors } from "quasar";
 export default {
   name: "dacFinancials",
   components: {
@@ -158,6 +183,7 @@ export default {
 
       trx_qeue: [
         {
+          status: 0,
           from: "eosdacdoshhq",
           to: "piecesnbitss",
           quantity: "1.0000 EOS",
@@ -167,6 +193,7 @@ export default {
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
         },
         {
+          status: 0,
           from: "eosdacdoshhq",
           to: "evilmikehere",
           quantity: "10000.0000 EOS",
@@ -176,6 +203,7 @@ export default {
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
         },
         {
+          status: 0,
           from: "eosdacdoshhq",
           to: "lukedactest1",
           quantity: "9999.0000 EOS",
@@ -194,6 +222,19 @@ export default {
     })
   },
   methods: {
+    addToQeue() {
+      let trx = {
+        status: 0,
+        from: "eosdacdoshhq",
+        to: "lukedactest1",
+        quantity: "9999.0000 EOS",
+        memo: "this is the memo of the transaction",
+        title: "Payment for wp #781",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+      };
+      this.trx_qeue.push(trx);
+    },
     async getPermissions() {
       for (let i = 0; i < this.financialaccounts.length; i++) {
         let finacc = this.financialaccounts[i];
@@ -209,6 +250,9 @@ export default {
     },
     async proposeTransfer(trx_index) {
       let trx_data = this.trx_qeue[trx_index];
+
+      //set status to the sending state 1
+      trx_data.status = 1;
 
       let from_account = this.financialaccounts.find(
         fa => fa.account == trx_data.from
@@ -243,7 +287,19 @@ export default {
       });
       if (res) {
         console.log(res);
+        trx_data.status = 2;
+      } else {
+        trx_data.status = 0;
       }
+    },
+    getThumbStyle() {
+      return {
+        right: "0px",
+        borderRadius: "0px",
+        background: colors.getBrand("primary-light"),
+        width: "5px",
+        opacity: 1
+      };
     }
   },
   mounted() {
