caa99648af6731c5379dbbdadc7809711763477e
unknown <kasperkasperfish@gmail.com>
2019-06-27 20:16:52 +0200

    pull isDark boolean from theme file

--- src/store/ui/state.js
+++ src/store/ui/state.js
@@ -1,6 +1,9 @@
+const theme_extensions_file = require("../../extensions/statics/config/theme.json");
+
 export default {
   //
   enableTransactionOverlay: true,
   showTransactionOverlay: false,
-  drawerIsOpen: true
+  drawerIsOpen: true,
+  isDark: theme_extensions_file.is_dark || true
 };
